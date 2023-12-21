5dc960fb20d252d604f3c292d89c1fb3957829c5
Kirill Ponomarenko <kirillponomarenko@MBP-Kirill.lan>
2023-12-21 16:07:11 +0300

    start

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Kirill Ponomarenko on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
